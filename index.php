00df0fed685125ae8d36933a75fff45fe9f580e0
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-15 09:16:38 -0300

    upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="logincontainer"> 
+    <h1>Acesse sua Conta</h1>
+    <form action="" method="POST">
+        <p>
+        <label> E-mail </label>
+        <input type="text" name="email">
+        </p>
+
+        <p>
+        <label> Senha </label>
+        <input type="password" name="senha">
+        </p>
+
+        <p>
+        <button type="submit"> Entrar </button>
+        </p>
+
+    </form>
+      </div>
+</body>
+</html>
